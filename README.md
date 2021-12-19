674e22f720d8a560c1b062750134acb95ae5ccaa
Umakant Vashishtha <umakantvashishtha98@gmail.com>
2021-12-19 15:42:34 +0530

    Tree - Same Tree

--- README.md
+++ README.md
@@ -100,7 +100,7 @@ String
 Tree
 
 - Maximum Depth of Binary Tree - https://leetcode.com/problems/maximum-depth-of-binary-tree/ ✅
-- Same Tree - https://leetcode.com/problems/same-tree/
+- Same Tree - https://leetcode.com/problems/same-tree/ ✅
 - Invert/Flip Binary Tree - https://leetcode.com/problems/invert-binary-tree/
 - Binary Tree Maximum Path Sum - https://leetcode.com/problems/binary-tree-maximum-path-sum/
 - Binary Tree Level Order Traversal - https://leetcode.com/problems/binary-tree-level-order-traversal/
---
+++ tree/binary-tree-same-tree.cpp
@@ -0,0 +1,23 @@
+/**
+ * struct TreeNode {
+ *    int val;
+ *    TreeNode *left;
+ *    TreeNode *right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p && !q) return true;
+        if((!p && q) || (p && !q)) return false;
+        if(p->val != q->val) {
+            return false;
+        }
+        if(isSameTree(p->left, q->left) == false) return false;
+        if(isSameTree(p->right, q->right) == false) return false;
+        return true;
+    }
+};
